1a2120931768f10c4953f385a9222ae8f9fb504d
Walid Mostafa <amadalioo980@gmail.com>
2025-12-31 17:21:58 +0200

    fix scroll page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation } from "react-router";
 import Header from "./components/Header/Header";
 import { Toaster } from "@/components/ui/sonner";
 import { ScrollArea } from "@/components/ui/scroll-area";
 
 function App() {
+  const { pathname } = useLocation();
   return (
     <main
       className="backdrop-blur-3xl"
@@ -13,8 +14,12 @@ function App() {
     >
       <Header />
 
-      <ScrollArea className="h-screen overflow-y-auto relative z-20">
-        <section className="container h-screen pt-16 pb-8">
+      <ScrollArea className="h-svh overflow-y-auto relative z-20">
+        <section
+          className={`container ${
+            pathname === "/" ? "h-svh" : "min-h-svh pt-16 pb-8"
+          }`}
+        >
           <Outlet />
         </section>
       </ScrollArea>
